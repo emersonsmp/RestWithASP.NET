692aa71c98aeed12d76918c17505b17dd71456ce
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2021-08-15 17:03:23 -0300

    Criado Repository e Business

---
+++ 05_RestWithASPNET_VS_Calculator_DB_integration/RestWithASPNET/RestWithASPNET/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,92 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        private MySQLContext _context;
+
+        public PersonRepositoryImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public List<PersonModel> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public PersonModel FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id == id);
+        }
+
+        public PersonModel Create(PersonModel person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return person;
+        }
+
+        public PersonModel Update(PersonModel person)
+        {
+            if (!Exists(person.Id)) return new PersonModel();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if(result != null) { 
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id == id);
+        }
+    }
+}
---
+++ 05_RestWithASPNET_VS_Calculator_DB_integration/RestWithASPNET/RestWithASPNET/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Services.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //criação do service do contexto de banco de dados----------------------
+            //["Busca-se a correspondencia no appsettings.json"]
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(
+                connection, new MySqlServerVersion(new Version(8, 0, 26)))
+            );
+            //----------------------------------------------------------------------
+
+            //Versionamento API
+            services.AddApiVersioning();
+
+            //Dependency injection
+            services.AddScoped<IPersonRepository, PersonServiceImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
