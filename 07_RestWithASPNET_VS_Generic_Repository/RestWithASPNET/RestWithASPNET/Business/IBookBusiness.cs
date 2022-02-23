636f2ac351e0a0f39726f9cfec5f3fb0ea387f48
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2022-02-22 22:24:32 -0300

    Modulo Value Object
    Modulo Value Object

---
+++ 07_RestWithASPNET_VS_Generic_Repository/RestWithASPNET/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
---
+++ 07_RestWithASPNET_VS_Generic_Repository/RestWithASPNET/RestWithASPNET/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,46 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Business.Implementations
+{
+    //CAMADA PARA COLOCAR AS REGRAS DE NEGOCIO E VALIDAÇÕES
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        public Person Create(Person person)
+        {
+            //REGRA EXEMPLO: SÓ ADD SE ANO DE NASC > 1900
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
