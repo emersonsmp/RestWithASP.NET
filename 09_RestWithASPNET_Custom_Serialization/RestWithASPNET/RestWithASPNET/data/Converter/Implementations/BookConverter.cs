4f646eb415579b6e9dfdd07c7567fcb445d6efe7
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2022-02-22 22:27:01 -0300

    Modulo Custom Serialization
    Modulo Custom Serialization

---
+++ 09_RestWithASPNET_Custom_Serialization/RestWithASPNET/RestWithASPNET/data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,51 @@
+﻿using RestWithASPNET.data.Converter.Contract;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.data.Converter.Implementations
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+            return new Book()
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LauchDate = origin.LauchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+            return new BookVO()
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LauchDate = origin.LauchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ 09_RestWithASPNET_Custom_Serialization/RestWithASPNET/RestWithASPNET/data/VO/PersonVO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNET.Data.VO
+{
+    public class PersonVO
+    {  
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("nome")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("sobrenome")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("endereco")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("sexo")]
+        public string Gender { get; set; }
+    }
+}
