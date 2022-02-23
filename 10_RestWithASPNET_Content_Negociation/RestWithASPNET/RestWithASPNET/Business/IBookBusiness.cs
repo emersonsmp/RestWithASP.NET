39b816c8d1900f7924a7bd550919fdbf6c9f2db3
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2022-02-22 22:27:46 -0300

    Modulo Content Negociation
    Modulo Content Negociation

---
+++ 10_RestWithASPNET_Content_Negociation/RestWithASPNET/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
---
+++ 10_RestWithASPNET_Content_Negociation/RestWithASPNET/RestWithASPNET/db/datasets/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `last_name`, `gender`) VALUES
+	(1, 'São paulo - SP', 'Airton', 'Sena', 'Male'),
+	(2, 'Archiano - Italy', 'Leonardo', 'Da Vinci', 'Male'),
+	(3, 'Porbandar - India', 'Mahatma', 'Gandhi', 'Male'),
+	(4, 'Kentucky - USA', 'Mahamed', 'Ali', 'Male'),
+	(5, 'Mvezo - South Africa', 'Nelson', 'Mandela', 'Male');
