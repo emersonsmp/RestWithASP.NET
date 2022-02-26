4854950541fe84bcab0342d413d59e278b4c09ad
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2022-02-26 17:43:09 -0300

    Adicionado swagger

---
+++ 11_RestWithASPNET_SWAGGER/RestWithASPNET/RestWithASPNET/data/VO/PersonVO.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNET.Hypermedia;
+using RestWithASPNET.Hypermedia.Abstract;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET.Data.VO
+{
+    public class PersonVO : ISupportHyperMedia
+    {  
+        public long Id { get; set; }
+        public string FirstName { get; set; }    
+        public string LastName { get; set; } 
+        public string Address { get; set; }  
+        public string Gender { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
